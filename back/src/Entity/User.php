c9c9380e8d6641c85450187505e064b01ce4d1a5
Cyril <cyrilguechi71@gmail.com>
2020-04-15 19:54:54 +0200

    Add make:user

--- back/src/Entity/User.php
+++ back/src/Entity/User.php
@@ -2,27 +2,32 @@
 
 namespace App\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Collection;
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
-use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  * @ApiResource
  */
-class User
+class User implements UserInterface
 {
-    /** 
+    /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
      * @groups("admin")
      */
     private $id;
-
     /**
+     * @ORM\Column(type="string", nullable=true)
+     * @groups("admin")
+     */
+    private $birthday;
+/**
      * @ORM\Column(type="string", length=60)
      * @groups("admin")
      */
@@ -56,15 +61,7 @@ class User
      * @ORM\Column(type="string", length=120)
      * @groups("admin")
      */
-    private $mail;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     * @groups("admin")
-     */
-    private $password;
-
-    /**
+  /**
      * @ORM\Column(type="string", length=35, nullable=true)
      * @groups("admin")
      */
@@ -87,24 +84,108 @@ class User
      * @ORM\OneToMany(targetEntity="App\Entity\Article", mappedBy="user")
      */
     private $article;
-
     /**
-     * @ORM\ManyToOne(targetEntity="App\Entity\Role", inversedBy="users")
-     * @ORM\JoinColumn(nullable=false)
+     * @ORM\Column(type="string", length=180, unique=true)
      * @groups("admin")
      */
-    private $role;
+    private $email;
+
+    /**
+     * @ORM\Column(type="json")
+     * @groups("admin")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     * @groups("admin")
+     */
+    private $password;
 
     public function __construct()
     {
         $this->article = new ArrayCollection();
     }
 
+
     public function getId(): ?int
     {
         return $this->id;
     }
 
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
     public function getFirstname(): ?string
     {
         return $this->firstname;
@@ -164,31 +245,6 @@ class User
 
         return $this;
     }
-
-    public function getMail(): ?string
-    {
-        return $this->mail;
-    }
-
-    public function setMail(string $mail): self
-    {
-        $this->mail = $mail;
-
-        return $this;
-    }
-
-    public function getPassword(): ?string
-    {
-        return $this->password;
-    }
-
-    public function setPassword(string $password): self
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
     public function getLicense(): ?string
     {
         return $this->license;
@@ -224,7 +280,6 @@ class User
 
         return $this;
     }
-
     /**
      * @return Collection|Article[]
      */
@@ -255,15 +310,14 @@ class User
 
         return $this;
     }
-
-    public function getRole(): ?Role
+    public function getBirthday(): ?\DateTimeInterface
     {
-        return $this->role;
+        return $this->birthday;
     }
 
-    public function setRole(?Role $role): self
+    public function setBirthday(\DateTimeInterface $birthday): self
     {
-        $this->role = $role;
+        $this->birthday = $birthday;
 
         return $this;
     }
---
+++ back/src/Migrations/Version20200415173554.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200415173554 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D649D60322AC');
+        $this->addSql('DROP TABLE role');
+        $this->addSql('DROP INDEX IDX_8D93D649D60322AC ON user');
+        $this->addSql('ALTER TABLE user ADD email VARCHAR(180) NOT NULL, ADD roles JSON NOT NULL, DROP role_id, DROP mail');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON user (email)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE role (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(15) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('DROP INDEX UNIQ_8D93D649E7927C74 ON user');
+        $this->addSql('ALTER TABLE user ADD role_id INT NOT NULL, ADD mail VARCHAR(120) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, DROP email, DROP roles');
+        $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D649D60322AC FOREIGN KEY (role_id) REFERENCES role (id)');
+        $this->addSql('CREATE INDEX IDX_8D93D649D60322AC ON user (role_id)');
+    }
+}
