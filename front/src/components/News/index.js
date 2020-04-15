bd4f37dd506d22273a30400a65a2a22de95676e7
Live session user <mint@etudiant.oclock.io>
2020-04-15 11:03:29 +0200

    part2

--- front/src/components/News/index.js
+++ front/src/components/News/index.js
@@ -5,20 +5,30 @@ import './style.scss';
 const News = () => (
     <div className="news">
         <h2 className="news__title"> Les Actualités</h2>
-        <div className="news__nav-search">
-            <div className="news__nav-search__nav">
-                <button className="news__nav-search__nav__tag">Athlètes</button>
-                <button className="news__nav-search__nav__tag">Club</button>
-                <button className="news__nav-search__nav__tag">Résultats</button>
+        <div className="news__container">
+            <div className="news__container__navsearch">
+                <div className="news__container__navsearch__nav">
+                    <button className="news__container__navsearch__nav__button">Athlètes</button>
+                    <button className="news__container__navsearch__nav__button">Club</button>
+                    <button className="news__container__navsearch__nav__button">Résultats</button>
+                </div>
+                <div className="news__container__navsearch__search">
+                    <form className="news__container__navsearch__search__form">
+                        <input className="news__container__navsearch__search__form__input" type="text" placeholder="Search"></input>
+                    </form>
+                </div>
             </div>
-            <div className="news__nav-search__search">
-            
+            <div className="news__container__articles">
+                <div className="news__container__articles__article">
+                    <img className="news__container__articles__article__image" src="" />
+                    <div className="news__container__articles__article__text">
+                        <button className="news__container__articles__article__text__tag"></button>
+                        <h3 className="news__container__articles__article__text__title">Titre</h3>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div className="news__articles">
-        
-        </div>
-        <div className="news__prev-next">
+            <div className="news__container__prevnext">
+            </div>
         </div>
     </div>
 );
