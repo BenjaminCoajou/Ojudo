a390d564226c0d187f0b1311156bfde7a2dc5338
Live session user <mint@etudiant.oclock.io>
2020-04-17 15:15:31 +0200

    init

---
+++ front/src/containers/Presentation.js
@@ -0,0 +1,8 @@
+import { connect } from 'react-redux';
+import Presentation from '../components/Presentation';
+
+const mapStateToProps = () => ({});
+
+const mapStateToDispatch = () => ({});
+
+export default connect(mapStateToProps, mapStateToDispatch)(Presentation);
