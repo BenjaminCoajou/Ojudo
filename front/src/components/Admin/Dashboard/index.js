048402c3921e3817f5a1d82b09744f2ac8dd5c7f
johan <johan.lm@hotmail.fr>
2020-04-18 18:32:29 +0200

    rename routes

--- front/src/components/Admin/Dashboard/index.js
+++ front/src/components/Admin/Dashboard/index.js
@@ -16,43 +16,43 @@ const images = [
       url: 'https://cdn.pixabay.com/photo/2017/10/12/22/41/antique-2846276_960_720.jpg',
       title: 'Utilisateurs',
       width: '600px',
-      link: '/user',
+      link: '/admin/user',
     },
     {
       url: 'https://cdn.pixabay.com/photo/2017/03/08/00/10/judo-2125606_960_720.jpg',
       title: 'Catégorie',
       width: '600px',
-      link: '/categorie'
+      link: '/admin/categorie'
     },
     {
         url: 'https://www.optionfinance.fr/fileadmin/_processed_/csm_img-event_54745635d1.jpg',
         title: 'Evénements',
         width: '600px',
-        link: '/event'
+        link: '/admin/event'
     },
     {
         url: 'https://cdn.pixabay.com/photo/2017/08/13/16/43/notebook-2637757_960_720.jpg',
         title: 'Articles et tags',
         width: '600px',
-        link: '/article'
+        link: '/admin/article'
     },
     {
         url: 'https://cdn2.vectorstock.com/i/1000x1000/26/21/clipboard-icon-male-person-profile-avatar-symbol-vector-21112621.jpg',
         title: 'Liste des utilisateurs',
         width: '600px',
-        link: '/userlist'
+        link: '/admin/userlist'
     },
     {
         url: 'https://cdn.svenskalag.se/teamdata/images/33035/484024/sponsor_730.jpg?0',
         title: 'Sponsors',
         width: '600px',
-        link: '/sponsors'
+        link: '/admin/sponsors'
     },
     {
         url: 'https://www.trelaze.fr/wp-content/uploads/2019/05/judo.jpg',
         title: 'Entrainement',
         width: '600px',
-        link: '/categorie/masculin'
+        link: '/admin/categorie/masculin'
     },
 
 
