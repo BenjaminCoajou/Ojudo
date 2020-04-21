345d5a5d1b7e4685b76b44c3a71990b8454c3c9e
Cyril <cyrilguechi71@gmail.com>
2020-04-21 19:50:18 +0200

    Add role in user entity

--- back/src/Migrations/Version20200421171007.php
+++ back/src/Migrations/Version20200421171007.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200421094513 extends AbstractMigration
+final class Version20200421171007 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,7 @@ final class Version20200421094513 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE article CHANGE user_id user_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE user CHANGE role_id role_id INT DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +30,6 @@ final class Version20200421094513 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE article CHANGE user_id user_id INT NOT NULL');
+        $this->addSql('ALTER TABLE user CHANGE role_id role_id INT NOT NULL');
     }
 }
