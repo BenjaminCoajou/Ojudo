d1ec8e08d819b2dd89b884b3228307afaf616f82
johan <johan.lm@hotmail.fr>
2020-04-23 23:56:59 +0200

    map leaflet integration

--- front/src/components/Events/index.js
+++ front/src/components/Events/index.js
@@ -1,8 +1,9 @@
 import React from "react";
+import L from 'leaflet';
 
 import Header from "../Header";
 import Footer from "../Footer";
-
+import Map from "./maps";
 import "./style.scss";
 
 
@@ -12,7 +13,12 @@ const Events = () => (
   <div>
     <Header />
     
-    
+
+    <div>
+    <Map />
+    </div>
+
+
     <Footer />
   </div>
 );
