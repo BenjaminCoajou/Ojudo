6fed62dbbecdf6cb0de4ccaab432444e0d465178
BenjaminCoajou <coajou.benjamin@gmail.com>
2020-04-27 16:27:42 +0200

    fetch sponsors from api and css on sponsor's card

--- front/src/components/Sponsors/index.js
+++ front/src/components/Sponsors/index.js
@@ -2,24 +2,28 @@ import React from 'react';
 
 import './style.scss';
 
-const Sponsors = () => (
-    
-    <div className="row">
-    <div className="example-1 card_sponsor">
-      <div className="wrapper_sponsors">
-        <div className="data">
-          <div className="content">
-            <h1 className="title">Nom de l'entreprise</h1>
-            <p className="title">sa description</p>
-            <p className="title">son adresse</p>
-            <p className="title">contact</p>
-          </div>
+const Sponsors = ({ list }) => (
 
+  <div className="row">
+    {list.map((sponsor) => (
+      <div className="example-1 card_sponsor">
+        <div className="wrapper_sponsors">
+          <div className="data">
+            <div className="content">
+              <h1 className="title-sponsor">{sponsor.title}</h1>
+              <p className="title-sponsors">{sponsor.content}</p>
+              <p className="title-sponsors">{sponsor.address}</p>
+              <p className="title-sponsors">{sponsor.telephone}</p>
+            </div>
+
+          </div>
         </div>
-      </div>
-    </div>
+
+      </div>))}
   </div>
-  
+
+
+
 );
 
 export default Sponsors;
--- front/src/reducers/index.js
+++ front/src/reducers/index.js
@@ -5,6 +5,7 @@ import login from './login';
 import users from './Admin/users';
 import articles from './Admin/articles';
 import presentation from './presentation';
+import sponsors from './sponsors'
 
 export default combineReducers({
   calendar,
@@ -12,4 +13,5 @@ export default combineReducers({
   users,
   articles,
   presentation,
+  sponsors,
 });
