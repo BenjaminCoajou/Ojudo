66b5ac959ace1de62b83aa4e404a45f8388b7fc1
Live session user <mint@etudiant.oclock.io>
2020-04-28 13:49:38 +0200

    change title and content

--- front/src/components/News/NewsCard1.js
+++ front/src/components/News/NewsCard1.js
@@ -3,18 +3,20 @@ import PropTypes from 'prop-types';
 
 import './style.scss';
 
-const NewsCard1 = ({content, picture,}) => {
+const NewsCard1 = ({title, content, picture,}) => {
 
     return(
         <div className="card card-color-0">
             <div className="border"></div>
             <img src={picture} />
-            <h1>{content}</h1>
+            <h1>{title}</h1>
+            {content}
         </div>
     );
 }
 
 NewsCard1.propTypes = {
+    title: PropTypes.string.isRequired,
     content: PropTypes.string.isRequired,
     picture: PropTypes.string.isRequired,
 };
--- front/src/components/News/NewsCard2.js
+++ front/src/components/News/NewsCard2.js
@@ -3,18 +3,20 @@ import PropTypes from 'prop-types';
 
 import './style.scss';
 
-const NewsCard2 = ({content, picture,}) => {
+const NewsCard2 = ({title, content, picture,}) => {
 
     return(
         <div className="card card-color-1">
             <div className="border"></div>
             <img src={picture} />
-            <h1>{content}</h1>
+            <h1>{title}</h1>
+            {content}
         </div>
     );
 }
 
 NewsCard2.propTypes = {
+    title: PropTypes.string.isRequired,
     content: PropTypes.string.isRequired,
     picture: PropTypes.string.isRequired,
 };
