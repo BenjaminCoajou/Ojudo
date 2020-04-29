ff0bcc95ad02de15f4382a9fda5436acef845156
BenjaminCoajou <coajou.benjamin@gmail.com>
2020-04-29 11:07:51 +0200

    fix

--- front/src/components/News/article.js
+++ front/src/components/News/article.js
@@ -1,7 +1,6 @@
 var initNews = () => {
-var cards, nCards, cover, openContent, openContentText, pageIsOpen = false,
-    openContentImage, closeContent, windowWidth, windowHeight, currentCard;
-
+  var cards, nCards, cover, openContent, openContentText, pageIsOpen = false,
+  openContentImage, closeContent, windowWidth, windowHeight, currentCard;
 // initiate the process
 /* init(); */
 
@@ -183,7 +182,7 @@ function resize() {
   windowHeight = window.innerHeight;
 }
 
-var paragraphText = '<p>Somebody once told me the world is gonna roll me. I ain\'t the sharpest tool in the shed. She was looking kind of dumb with her finger and her thumb in the shape of an "L" on her forehead. Well the years start coming and they don\'t stop coming. Fed to the rules and I hit the ground running. Didn\'t make sense not to live for fun. Your brain gets smart but your head gets dumb. So much to do, so much to see. So what\'s wrong with taking the back streets? You\'ll never know if you don\'t go. You\'ll never shine if you don\'t glow.</p><p>Hey now, you\'re an all-star, get your game on, go play. Hey now, you\'re a rock star, get the show on, get paid. And all that glitters is gold. Only shooting stars break the mold.</p><p>It\'s a cool place and they say it gets colder. You\'re bundled up now, wait till you get older. But the meteor men beg to differ. Judging by the hole in the satellite picture. The ice we skate is getting pretty thin. The water\'s getting warm so you might as well swim. My world\'s on fire, how about yours? That\'s the way I like it and I never get bored.</p>';
+var paragraphText = '<p>Donec libero libero, aliquam vel dictum id, varius at libero. Maecenas et risus ac metus ornare sollicitudin ut quis orci. Curabitur sagittis risus vel molestie condimentum. Donec nec porta diam. Quisque iaculis semper purus vitae lacinia. Pellentesque condimentum metus consectetur, aliquam diam sit amet, aliquet sapien. Maecenas tempus risus eget leo ullamcorper ornare. Sed faucibus urna sed purus tristique aliquet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi posuere mi augue, ac cursus odio rhoncus in. Suspendisse id tellus aliquam, rutrum sapien fringilla, mollis nunc. Curabitur porta, sapien quis faucibus gravida, leo ipsum tincidunt mauris, ut viverra nisl nisi ultrices elit. Integer non tellus consequat dolor vulputate maximus. Nam volutpat facilisis mi vitae eleifend. Proin aliquam turpis non eleifend tincidunt.</p>';
 return init;
 };
 
