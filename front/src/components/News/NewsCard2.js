e4a96e7b36b904ad53d3d6ca572b40ad72130aa2
Live session user <mint@etudiant.oclock.io>
2020-04-30 12:07:37 +0200

    picture not found

--- front/src/components/News/NewsCard2.js
+++ front/src/components/News/NewsCard2.js
@@ -8,7 +8,7 @@ const NewsCard2 = ({title, picture, content}) => {
     return(
         <div className="card card-color-1">
             <div className="border"></div>
-            <img src={picture} />
+            <img src={picture.filePath} />
             <h1>{title}</h1>
             <p hidden id='text-content-article'>{content}</p>
         </div>
@@ -17,7 +17,11 @@ const NewsCard2 = ({title, picture, content}) => {
 
 NewsCard2.propTypes = {
     title: PropTypes.string.isRequired,
-    picture: PropTypes.string.isRequired,
+    picture: PropTypes.arrayOf(
+        PropTypes.shape({
+          filPath: PropTypes.string.isRequired,
+        }),
+      ).isRequired,
     content: PropTypes.string.isRequired,
 };
 
